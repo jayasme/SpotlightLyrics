13ab4764bd5d65f278a2cd07bae780a20c3b964b
jayasme <sunshine121981@126.com>
2018-03-30 17:40:45 +0800

    Add test lrc file

--- SpotlightLyrics_Demo/SpotlightLyrics_Demo/ViewController.swift
+++ SpotlightLyrics_Demo/SpotlightLyrics_Demo/ViewController.swift
@@ -16,7 +16,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        lyricsView.lyrics = "dsddsdsd"
+        // Read the test LRC file
+        guard
+            let path = Bundle.main.path(forResource: "Santa Monica Dream", ofType: "lrc"),
+            let stream = InputStream(fileAtPath: path)
+        else {
+            return
+        }
+        
+        var data = Data.init()
+        
+        stream.open()
+        let bufferSize = 1024
+        let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: bufferSize)
+        while (stream.hasBytesAvailable) {
+            let read = stream.read(buffer, maxLength: bufferSize)
+            data.append(buffer, count: read)
+        }
+        stream.close()
+        buffer.deallocate(capacity: bufferSize)
+        let lyrics = String(data: data, encoding: .utf8)
+        
+        // Initialize the SpotlightLyrics view
+        lyricsView.lyrics = lyrics
         lyricsView.font = UIFont.systemFont(ofSize: 15)
         lyricsView.textColor = UIColor.black
     }
